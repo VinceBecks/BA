e5d2f5d1f8df00022fd19da15f6f0b2e01c511bb
Vincent Becker <vincent.becker@openknowledge.de>
2019-03-13 19:16:28 +0100

    Create classes UnlikeTweetIT and UnlikeTweetSteps

---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/stepDefinition/UnlikeTweetSteps.java
@@ -0,0 +1,11 @@
+package de.openknowledge.playground.api.rest.security.stepDefinition;
+
+import de.openknowledge.playground.api.rest.security.supportCode.SharedDomain;
+
+public class UnlikeTweetSteps {
+    private SharedDomain domain;
+
+    public UnlikeTweetSteps (SharedDomain domain) {
+        this.domain = domain;
+    }
+}

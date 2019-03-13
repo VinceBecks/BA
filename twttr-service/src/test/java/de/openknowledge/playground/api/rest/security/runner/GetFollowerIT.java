63da289b1e6e4cb3bbb2fef9333dc0a79ec7f339
Vincent Becker <vincent.becker@openknowledge.de>
2019-03-13 10:42:16 +0100

    Create classes GetFollowerIT and GetFollowerSteps

---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/runner/GetFollowerIT.java
@@ -0,0 +1,4 @@
+package de.openknowledge.playground.api.rest.security.runner;
+
+public class GetFollowerIT {
+}
---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/stepDefinition/GetFollowerSteps.java
@@ -0,0 +1,12 @@
+package de.openknowledge.playground.api.rest.security.stepDefinition;
+
+import de.openknowledge.playground.api.rest.security.supportCode.SharedDomain;
+
+public class GetFollowerSteps {
+
+    private SharedDomain domain;
+
+    public GetFollowerSteps(SharedDomain domain) {
+        this.domain = domain;
+    }
+}

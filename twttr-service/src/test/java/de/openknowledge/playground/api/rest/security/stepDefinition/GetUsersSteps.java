f12351545f68b9854c295101db8233bcf73962f0
Vincent Becker <vincent.becker@openknowledge.de>
2019-03-13 21:21:42 +0100

    Create classes GetUsersIT and GetUsersSteps

---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/stepDefinition/GetUsersSteps.java
@@ -0,0 +1,12 @@
+package de.openknowledge.playground.api.rest.security.stepDefinition;
+
+import de.openknowledge.playground.api.rest.security.supportCode.SharedDomain;
+
+public class GetUsersSteps {
+
+    private SharedDomain domain;
+
+    public GetUsersSteps (SharedDomain domain) {
+        this.domain = domain;
+    }
+}

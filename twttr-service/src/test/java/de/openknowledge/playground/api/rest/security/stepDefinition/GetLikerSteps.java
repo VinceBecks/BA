8fba005b93b6443c0c3cf0f1294fdc13d55ff136
Vincent Becker <vincent.becker@openknowledge.de>
2019-03-13 11:34:03 +0100

    Created classes GetLikerIT and GetLikerSteps

---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/stepDefinition/GetLikerSteps.java
@@ -0,0 +1,11 @@
+package de.openknowledge.playground.api.rest.security.stepDefinition;
+
+import de.openknowledge.playground.api.rest.security.supportCode.SharedDomain;
+
+public class GetLikerSteps {
+
+    private SharedDomain domain;
+    public GetLikerSteps(SharedDomain domain) {
+        this.domain = domain;
+    }
+}

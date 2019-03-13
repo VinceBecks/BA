a0e670c09fb8e5eb8b998daba496124103d13e57
Vincent Becker <vincent.becker@openknowledge.de>
2019-03-13 18:59:59 +0100

    Create classes UnfollowUserIT and UnfollowUserSteps

---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/stepDefinition/UnfollowUserSteps.java
@@ -0,0 +1,10 @@
+package de.openknowledge.playground.api.rest.security.stepDefinition;
+
+import de.openknowledge.playground.api.rest.security.supportCode.SharedDomain;
+
+public class UnfollowUserSteps {
+    private SharedDomain domain;
+    public UnfollowUserSteps (SharedDomain domain) {
+        this.domain =domain;
+    }
+}

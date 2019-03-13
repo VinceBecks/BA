920c7a36b0c68bae7ab33f0700ca3559bb5652ba
Vincent Becker <vincent.becker@openknowledge.de>
2019-03-13 18:38:52 +0100

    Create classes RetweetTweetIT and RetweetTweetSteps

---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/stepDefinition/RetweetTweetSteps.java
@@ -0,0 +1,13 @@
+package de.openknowledge.playground.api.rest.security.stepDefinition;
+
+import de.openknowledge.playground.api.rest.security.supportCode.SharedDomain;
+
+public class RetweetTweetSteps {
+    private SharedDomain domain;
+
+    public RetweetTweetSteps (SharedDomain domain) {
+        this.domain = domain;
+    }
+
+
+}

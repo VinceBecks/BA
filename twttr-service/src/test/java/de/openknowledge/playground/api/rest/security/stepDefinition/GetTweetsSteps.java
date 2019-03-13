1e3e8c05242439f27182c1af6b4e13975523b718
Vincent Becker <vincent.becker@openknowledge.de>
2019-03-13 15:22:55 +0100

    Create classes GetTweetsIT and GetTweetsSteps

---
+++ twttr-service/src/test/java/de/openknowledge/playground/api/rest/security/stepDefinition/GetTweetsSteps.java
@@ -0,0 +1,12 @@
+package de.openknowledge.playground.api.rest.security.stepDefinition;
+
+import de.openknowledge.playground.api.rest.security.supportCode.SharedDomain;
+
+public class GetTweetsSteps {
+    private SharedDomain domain;
+
+    public GetTweetsSteps (SharedDomain domain) {
+        this.domain = domain;
+    }
+
+}
